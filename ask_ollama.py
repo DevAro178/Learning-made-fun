7190c3527a4078d1f29460562022c3cded60e5e1
Muhammad Ammad <me.ammad1786@gmail.com>
2024-08-17 20:27:48 +0500

    Everything is working fine now

---
+++ ask_ollama.py
@@ -0,0 +1,58 @@
+import argparse
+from langchain_community.vectorstores import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_ollama import ChatOllama
+
+CHROMA_PATH = "./tmp/chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+
+    # Prepare the DB.
+    model_name = "BAAI/bge-small-en"
+    model_kwargs = {"device": "cpu"}
+    encode_kwargs = {"normalize_embeddings": True}
+    embedding = HuggingFaceBgeEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs,
+    )
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding)
+
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    print(prompt)
+    
+    model = ChatOllama(base_url="http://15.207.88.163:11434",model="dolphin-mistral:latest")
+    response_text = model.invoke(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+
+
+if __name__ == "__main__":
+    main()
